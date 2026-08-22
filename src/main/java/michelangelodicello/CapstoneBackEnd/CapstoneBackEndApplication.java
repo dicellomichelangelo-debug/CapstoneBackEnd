9915ab0ba5f1d27d76f7069ccea2270b56dc1496
dicellomichelangelo-debug <dicellomichelangelo@gmail.com>
2026-08-22 10:52:05 +0200

    Initial commit

---
+++ src/main/java/michelangelodicello/CapstoneBackEnd/CapstoneBackEndApplication.java
@@ -0,0 +1,13 @@
+package michelangelodicello.CapstoneBackEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneBackEndApplication.class, args);
+	}
+
+}
